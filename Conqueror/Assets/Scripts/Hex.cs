02c4d653544f16500aea456f7b265e100ab3cfee
szucsmik96 <szucsmik96@gmail.com>
2018-06-05 00:04:55 +0100

    initial commit

---
+++ Conqueror/Assets/Scripts/Hex.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hex {
+
+	public Hex(int q, int r){
+		this.Q = q;
+		this.R = r;
+		this.S = -(q + r);
+	}
+
+	public readonly int Q;
+	public readonly int R;
+	public readonly int S;
+
+	readonly float WIDTH_MULTIPLIER=Mathf.Sqrt(3)/2;
+
+	public Vector3 Position(){
+
+		float radius = 1f;
+		float height = radius * 2;
+		float width = WIDTH_MULTIPLIER * height;
+
+		float vert = height * 0.75f;
+		float horiz = width;
+
+		return new Vector3 (
+			horiz*(this.Q+this.R/2f),
+			0,
+			vert*this.R
+		);
+	}
+}
--- Conqueror/Assets/Scripts/HexMap.cs
+++ Conqueror/Assets/Scripts/HexMap.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 public class HexMap : MonoBehaviour {
 
 	public GameObject HexPrefab;
+	Hex h;
 
 	// Use this for initialization
 	void Start () {
 		GenerateMap ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
 		
-	}
-
 	void GenerateMap(){
 		for(int column =0; column<10; column++){
 			for (int row = 0; row < 10; row++) {
-				Instantiate (HexPrefab, new Vector3 (column, 0, row), Quaternion.identity, this.transform);
+				h = new Hex (column, row);
+
+				Instantiate (HexPrefab, h.Position(), Quaternion.identity, this.transform);
 			}
 		}
 	}
